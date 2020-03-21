d67603adfc3a62c437274558072fbb80c0dbbf49
caique camargo <caique.de.camargo@hotmail.com>
2020-03-21 15:03:04 -0300

    adicionando novas paginas

---
+++ web/js/modules/asideMenu.js
@@ -0,0 +1,11 @@
+import Dom from "./domInteractive.js";
+
+export default function initAsideMenu() {
+  const asideButton = new Dom('[data-aside-button]').element();
+  const aside = new Dom('[data-aside]').element();
+
+  asideButton.addEventListener('click', () => {
+    aside.classList.toggle('active');
+  }
+  )
+}
--- web/js/script.js
+++ web/js/script.js
@@ -1,9 +1,12 @@
-import initAnimateScroll from "./modules/animateScroll.js";
-import initAnimateMenu from "./modules/animateMenu.js";
-import initMobileMenu from "./modules/mobileMenu.js";
+// import initAnimateScroll from "./modules/animateScroll.js";
+// import initAnimateMenu from "./modules/animateMenu.js";
+// import initMobileMenu from "./modules/mobileMenu.js";
+import initAsideMenu from "./modules/asideMenu.js";
 
-initAnimateScroll();
-window.onload = initAnimateMenu;
-initMobileMenu();
+// initAnimateScroll();
+// window.onload = initAnimateMenu;
+// initMobileMenu();
 
+const myPage = window.location.pathname.split("/").pop();
+if (myPage != "index.html") initAsideMenu();
 
