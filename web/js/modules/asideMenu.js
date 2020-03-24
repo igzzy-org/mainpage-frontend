2eb8d8fc065f509bddb26f984a4d275cb1b1cd87
caiquecamargo <caique.de.camargo@hotmail.com>
2020-03-24 17:49:14 -0300

    att

--- web/js/modules/asideMenu.js
+++ web/js/modules/asideMenu.js
@@ -1,12 +1,14 @@
 import Dom from "./domInteractive.js";
 
 export default function initAsideMenu() {
-  const asideButton = new Dom('[data-aside-button]').element();
+  const asideButton = new Dom('[data-aside-button]').listElements();
   const aside = new Dom('[data-aside]').element();
 
-  asideButton.addEventListener('click', () => {
+  asideButton[0].addEventListener('click', () => {
     aside.classList.toggle('active');
-    asideButton.classList.toggle('active');
+    asideButton.forEach(btn => {
+      btn.classList.toggle('active');
+    })
   }
   )
 }
