1c7d7dcf8db5b4324d1270e9dd898e1278eb67ff
caiquecamargo <caique.de.camargo@hotmail.com>
2020-03-25 18:15:53 -0300

    ajustando parametros js

--- web/js/modules/asideMenu.js
+++ web/js/modules/asideMenu.js
@@ -1,14 +1,22 @@
 import Dom from "./domInteractive.js";
+import outsideClick from "./outsideClick.js";
 
 export default function initAsideMenu() {
-  const asideButton = new Dom('[data-aside-button]').listElements();
-  const aside = new Dom('[data-aside]').element();
+  const mainMenu = new Dom('[data-aside="main-menu"]').element();
+  const mobileMenu = new Dom('[data-aside="menu-mobile"]').element();
+  const menuSandwich = new Dom('[data-aside="menu-sandwich"]').element();
+  const html = document.documentElement;
 
-  asideButton[0].addEventListener('click', () => {
-    aside.classList.toggle('active');
-    asideButton.forEach(btn => {
-      btn.classList.toggle('active');
+  menuSandwich.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    menuSandwich.classList.toggle('active');
+  });
+
+  mainMenu.addEventListener('click', event => {
+    event.preventDefault();
+    mainMenu.classList.toggle('active');
+    outsideClick(mainMenu, ['click'], () => {
+      mainMenu.classList.remove('active');
     })
-  }
-  )
+  })
 }
